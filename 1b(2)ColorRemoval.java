bcd3b1f4698ca3ad8713f6461c5164ebc01cc6f7
trishaks <ishaaa342@gmail.com>
2025-05-18 13:18:11 +0530

    Create 1b(2)ColorRemoval.java

---
+++ 1b(2)ColorRemoval.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ColorRemoval {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.remove(2);
+		System.out.println(ls);
+		ls.remove("Blue");
+		System.out.println(ls);
+	}
+
+}
