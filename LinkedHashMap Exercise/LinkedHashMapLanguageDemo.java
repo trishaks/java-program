c8f9dee0323eaff53c794cc5c284d7688c094f9d
trishaks <ishaaa342@gmail.com>
2025-05-22 21:36:13 +0530

    Create LinkedHashMapLanguageDemo.java

---
+++ LinkedHashMap Exercise/LinkedHashMapLanguageDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.TreeMap;
+
+public class LinkedHashMapLanguageDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<String,Integer> programminglanguages = new LinkedHashMap<>();
+		programminglanguages.put("Java", 1995);
+		programminglanguages.put("Python", 1991);
+		programminglanguages.put("JavaScript", 1995);
+		programminglanguages.put("C++", 1985);
+		System.out.println(programminglanguages);
+	}
+
+}
