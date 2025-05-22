6f31df25fb35dcc469ccd345503ec751dc6bc6f3
trishaks <ishaaa342@gmail.com>
2025-05-22 21:42:22 +0530

    Update and rename LinkedHashMapEldestRemovalDemo.java to TreeMapClearAndReAddDemo.java

---
+++ TreeMapExercise/TreeMapClearAndReAddDemo.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.*;
+
+public class TreeMapClearAndReAddDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map< Integer,String> Map = new TreeMap<>();
+		// 1. Basic Operations
+		// Adding elements
+		Map.put(1,"Apple");
+		Map.put(2,"Strawberry");
+		Map.put(3,"Pear");
+		Map.put(4,"Cucumber");
+		Map.put(5,"Grapes");
+		Map.clear();
+		System.out.println("After removing All elements size : "+Map.size());
+		Map.put(1,"Apple");
+		Map.put(2,"Strawberry");
+		Map.put(3,"Pear");
+		Map.put(4,"Cucumber");
+		Map.put(5,"Grapes");
+		System.out.println("After Adding All elements size : "+Map.size());
+
+	}
+
+}
